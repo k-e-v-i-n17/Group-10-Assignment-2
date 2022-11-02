ecc62dd2897045c046d1cc9531486ee752895e31
Adam Beatty <beattyad@tcd.ie>
2022-11-02 16:57:19 +0000

    maven setup created for github actions

--- src/test/java/Group10CalculatorTest.java
+++ src/test/java/Group10CalculatorTest.java
@@ -1,7 +1,12 @@
+package test.java;
+
 import static org.junit.Assert.assertEquals;
 //import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.*;
+
+import main.java.Group10Calculator;
+
 import java.util.*;
 
 public class Group10CalculatorTest {
@@ -39,27 +44,28 @@ public class Group10CalculatorTest {
     @Test
     public void invalidExpression() {
         org.junit.Assert.assertEquals(0, Group10Calculator.evaluate("5+2ergerg"));
-  }
-  @Test
-  public void invalidDouble(){
-    org.junit.Assert.assertEquals(0, Group10Calculator.evaluate("5.2+4.3"));
-  }
-  @Test 
-  public void Power(){
-    org.junit.Assert.assertEquals(4, Group10Calculator.evaluate("2^2"));
-  }
-  @Test 
-  public void Divide(){
-    org.junit.Assert.assertEquals(8, Group10Calculator.evaluate("16/2"));
-  }
-  /*@Test 
+    }
+    @Test
+    public void invalidDouble(){
+        org.junit.Assert.assertEquals(0, Group10Calculator.evaluate("5.2+4.3"));
+    }
+    @Test
+    public void Power(){
+        org.junit.Assert.assertEquals(4, Group10Calculator.evaluate("2^2"));
+    }
+    @Test
+    public void Divide(){
+        org.junit.Assert.assertEquals(8, Group10Calculator.evaluate("16/2"));
+    }
+  /*@Test
   public void expBasic(){
     org.junit.Assert.assertEquals(54.598, Group10Calculator.evaluate("exp(4)"));
   }
-  @Test 
+  @Test
   public void expBasic(){
     org.junit.Assert.assertEquals(30.786, Group10Calculator.evaluate("3+5*exp(4.2)/(5+7)"));
   }
   */
 
 }
+
